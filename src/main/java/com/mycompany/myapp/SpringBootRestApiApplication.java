033c174ef4cb6c88d5cbfa9beb9c993545383f4d
Robert Rong <robert.rong@agile-solutions.ch>
2019-08-17 14:04:53 +0200

    demo logging

--- src/main/java/com/mycompany/myapp/SpringBootRestApiApplication.java
+++ src/main/java/com/mycompany/myapp/SpringBootRestApiApplication.java
@@ -1,12 +1,21 @@
 package com.mycompany.myapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SpringBootRestApiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootRestApiApplication.class);
+	
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestApiApplication.class, args);
+        
+        logger.debug("Logging from demo app");
+        logger.debug("Logging from demo app");
+        logger.debug("Logging from demo app");
     }
 }
